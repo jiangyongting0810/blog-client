b1698cdd095224316995cb912ca5971adbc8af58
YongTing <jiangyongting0810@gmail.com>
2023-03-05 12:24:19 +0800

    引入markdown插件，接口修改

--- src/pages/Detail/template.vue
+++ src/pages/Detail/template.vue
@@ -1,15 +1,14 @@
 <template>
   <div class="detail">
     <section class="user-info">
-      <img
-        class="avatar"
-        src="https://s2.loli.net/2023/02/27/dOPhTWEpbQe6B4I.jpg"
-        alt=""
-      />
-      <h3>前端大揭秘</h3>
-      <p><router-link to="/user">永挺</router-link>发布于3天前</p>
+      <img class="avatar" :src="user.avatar" :alt="user.username" />
+      <h3>{{ title }}</h3>
+      <p>
+        <router-link :to="`/user/${user.id}`">{{ user.name }}</router-link
+        >{{ createdAt }}
+      </p>
     </section>
-    <section class="article"></section>
+    <section class="article" v-html="markdown"></section>
   </div>
 </template>
 
--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import Vue from "vue";
 import Router from "vue-router";
+
 import Login from "@/pages/Login/template.vue";
 import Create from "@/pages/Create/template.vue";
 import Detail from "@/pages/Detail/template.vue";
@@ -9,6 +10,7 @@ import My from "@/pages/My/template.vue";
 import Register from "@/pages/Register/template.vue";
 import User from "@/pages/User/template.vue";
 import store from "../store";
+
 window.store = store;
 
 Vue.use(Router);
@@ -52,6 +54,46 @@ const router = new Router({
     }
   ]
 });
+
+// const router = new Router({
+//   routes: [
+//     {
+//       path: "/",
+//       component: () => import("@/pages/Index/template.vue")
+//     },
+//     {
+//       path: "/login",
+//       component: () => import("@/pages/Login/template.vue")
+//     },
+//     {
+//       path: "/detail/:blogId",
+//       component: () => import("@/pages/Detail/template.vue")
+//     },
+//     {
+//       path: "/edit/:blogId",
+//       component: () => import("@/pages/Edit/template.vue"),
+//       meta: { requiresAuth: true }
+//     },
+//     {
+//       path: "/create",
+//       component: () => import("@/pages/Create/template.vue"),
+//       meta: { requiresAuth: true }
+//     },
+//     {
+//       path: "/user/:userId",
+//       component: () => import("@/pages/User/template.vue")
+//     },
+//     {
+//       path: "/my",
+//       component: () => import("@/pages/My/template.vue"),
+//       meta: { requiresAuth: true }
+//     },
+//     {
+//       path: "/register",
+//       component: () => import("@/pages/Register/template.vue")
+//     }
+//   ]
+// });
 
 router.beforeEach((to, from, next) => {
   if (to.matched.some(record => record.meta.requiresAuth)) {
