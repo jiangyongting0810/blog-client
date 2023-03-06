2ad8742256b78d869f3780a36c7f57aaa14eabdc
YongTing <jiangyongting0810@gmail.com>
2023-03-06 22:30:40 +0800

    user page

--- src/pages/User/template.vue
+++ src/pages/User/template.vue
@@ -1,9 +1,40 @@
 <template>
-  <div class="user">用户</div>
+  <div class="user">
+    <section class="user-info">
+      <img class="avatar" :src="user.avatar" :alt="user.username" />
+      <h3>{{ user.username }}</h3>
+    </section>
+    <section>
+      <router-link
+        :to="`/detail/${blog.id}`"
+        class="item"
+        v-for="blog in blogs"
+        :key="blog.id"
+      >
+        <div class="date">
+          <span class="day">{{ splitDate(blog.createdAt).date }}</span>
+          <span class="month">{{ splitDate(blog.createdAt).month }}月</span>
+          <span class="year">{{ splitDate(blog.createdAt).year }}</span>
+        </div>
+        <h3>{{ blog.title }}</h3>
+        <p>
+          {{ blog.description }}
+        </p>
+      </router-link>
+    </section>
+    <section>
+      <el-pagination
+        background
+        layout="prev, pager, next"
+        :total="total"
+        @current-change="onPageChange"
+      >
+      </el-pagination>
+    </section>
+  </div>
 </template>
 
 <script src="./template.js">
 </script>
 
-<style scoped src="./template.css">
-</style>
+<style src="../My/template.less" lang="less"></style>
