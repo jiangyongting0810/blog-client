fb0dde391264a6eee69564bf451f8fc06c776268
YongTing <jiangyongting0810@gmail.com>
2023-03-06 23:04:59 +0800

    my page

--- src/pages/My/template.vue
+++ src/pages/My/template.vue
@@ -1,37 +1,48 @@
 <template>
   <div class="my">
     <section class="user-info">
-      <img
-        class="avatar"
-        src="https://s2.loli.net/2023/02/27/dOPhTWEpbQe6B4I.jpg"
-        alt=""
-      />
-      <h3>永挺</h3>
+      <img class="avatar" :src="user.avatar" :alt="user.username" />
+      <h3>{{ user.username }}</h3>
     </section>
     <section>
-      <div class="item">
+      <router-link
+        :to="`/detail/${blog.id}`"
+        class="item"
+        v-for="blog in blogs"
+        :key="blog.id"
+      >
         <div class="date">
-          <span class="day">20</span>
-          <span class="month">5月</span>
-          <span class="year">2018</span>
+          <span class="day">{{ splitDate(blog.createdAt).date }}</span>
+          <span class="month">{{ splitDate(blog.createdAt).month }}月</span>
+          <span class="year">{{ splitDate(blog.createdAt).year }}</span>
         </div>
-        <h3>前端大揭秘</h3>
+        <h3>{{ blog.title }}</h3>
         <p>
-          本文以个简单的文件读写为例，讲解了异步的不同写法，包括
-          普通的callback.ES2016中的Promise和Generator、Node用于解决回调的co模块、ES2017中的async/await。适合初步接触
-          Node.js以及少量ES6语法的同学阅读。
-          本文以个简单的文件读写为例，讲解了异步的不同写法，包括
-          普通的callback.ES2016中的Promise和Generator、Node用于解决回调的co模块、ES2017中的async/await。适合初步接触
-          Node.js以及少量ES6语法的同学阅读。
+          {{ blog.description }}
         </p>
         <div class="actions">
-          <router-link to="/edit"><button>编辑</button></router-link>
-          <a href="#"><button>删除</button></a>
+          <router-link :to="`/edit/${blog.id}}`"
+            ><button>编辑</button></router-link
+          >
+          <!--@click.prevent 防止闪动 -->
+          <a href="#"
+            ><button @click.prevent="onDelete(blog.id)">删除</button></a
+          >
         </div>
-      </div>
+      </router-link>
+    </section>
+    <section>
+      <el-pagination
+        background
+        layout="prev, pager, next"
+        :total="total"
+        @current-change="onPageChange"
+      >
+      </el-pagination>
     </section>
   </div>
 </template>
+
 
 <script src="./template.js">
 </script>
